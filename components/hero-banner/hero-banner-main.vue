180c0a5beb7361bb23af91f4e50fc58f4e5751a3
Donny Pratama <donnypratama1024@gmail.com>
2023-11-21 20:19:23 +0700

    fix: carousel height

--- components/hero-banner/hero-banner-main.vue
+++ components/hero-banner/hero-banner-main.vue
@@ -1,9 +1,10 @@
 <template>
   <section class="tp-slider-area p-relative z-index-1">
     <Swiper
+      loop
+      autoplay
       :slidesPerView="1"
       :spaceBetween="30"
-      :loop="false"
       :navigation="{
         nextEl: '.tp-slider-button-next',
         prevEl: '.tp-slider-button-prev',
@@ -12,8 +13,7 @@
         el: '.tp-slider-dot',
         clickable: true,
       }"
-      :effect="'fade'"
-      :modules="[Navigation, Pagination, EffectFade]"
+      :modules="[Navigation, Pagination, Autoplay]"
       :onSlideChange="(swiper) => handleActiveIndex(swiper.activeIndex)"
       :class="`tp-slider-active tp-slider-variation swiper-container ${
         isActive ? 'is-light' : ''
@@ -22,66 +22,9 @@
       <SwiperSlide
         v-for="(item, i) in sliderData"
         :key="i"
-        :class="`tp-slider-item tp-slider-height d-flex align-items-center ${
-          item?.green_bg ? 'green-dark-bg' : item?.is_light ? 'is-light' : ''
-        }`"
-        :style="item.is_light && `background-color:#E3EDF6`"
+        class="tp-slider-item tp-slider-height d-flex align-items-center"
       >
-        <div class="tp-slider-shape">
-          <img
-            class="tp-slider-shape-1"
-            src="/img/slider/shape/slider-shape-1.png"
-            alt="slider-shape"
-          />
-          <img
-            class="tp-slider-shape-2"
-            src="/img/slider/shape/slider-shape-2.png"
-            alt="slider-shape"
-          />
-          <img
-            class="tp-slider-shape-3"
-            src="/img/slider/shape/slider-shape-3.png"
-            alt="slider-shape"
-          />
-          <img
-            class="tp-slider-shape-4"
-            src="/img/slider/shape/slider-shape-4.png"
-            alt="slider-shape"
-          />
-        </div>
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-xl-5 col-lg-6 col-md-6">
-              <div class="tp-slider-content p-relative z-index-1">
-                <span
-                  >{{ item.pre_title.text }}
-                  <b>Rp{{ item.pre_title.price.toFixed(2) }}</b></span
-                >
-                <h3 class="tp-slider-title">{{ item.title }}</h3>
-                <p>
-                  {{ item.subtitle.text_1 }}
-                  <span
-                    >-{{ item.subtitle.percent }}%
-                    <SvgOfferLine />
-                  </span>
-                  {{ item.subtitle.text_2 }}
-                </p>
-
-                <div class="tp-slider-btn">
-                  <nuxt-link href="/shop" class="tp-btn tp-btn-2 tp-btn-white">
-                    Shop Now
-                    <SvgRightArrow />
-                  </nuxt-link>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-7 col-lg-6 col-md-6">
-              <div class="tp-slider-thumb text-end">
-                <img :src="item.img" alt="slider-img" />
-              </div>
-            </div>
-          </div>
-        </div>
+        <img :src="item.img" :alt="`slider-${i}`" class="img-fluid" />
       </SwiperSlide>
       <div class="tp-slider-arrow tp-swiper-arrow d-none d-lg-block">
         <button type="button" class="tp-slider-button-prev">
@@ -98,62 +41,17 @@
 
 <script setup lang="ts">
 import { Swiper, SwiperSlide } from "swiper/vue";
-import { Navigation, Pagination, EffectFade } from "swiper/modules";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 // type
 type ISliderData = {
   id: number;
-  pre_title: {
-    text: string;
-    price: number;
-  };
-  title: string;
-  subtitle: {
-    text_1: string;
-    percent: number;
-    text_2: string;
-  };
   img: string;
-  green_bg?: boolean;
-  is_light?: boolean;
 };
 // slider data
 const sliderData: ISliderData[] = [
-  {
-    id: 1,
-    pre_title: { text: "Starting at", price: 274 },
-    title: "The best tablet Collection 2023",
-    subtitle: {
-      text_1: "Exclusive offer ",
-      percent: 35,
-      text_2: "off this week",
-    },
-    img: "/img/slider/slider-img-1.png",
-    green_bg: true,
-  },
-  {
-    id: 2,
-    pre_title: { text: "Starting at", price: 999 },
-    title: "The best note book collection 2023",
-    subtitle: {
-      text_1: "Exclusive offer ",
-      percent: 10,
-      text_2: "off this week",
-    },
-    img: "/img/slider/slider-img-2.png",
-    green_bg: true,
-  },
-  {
-    id: 3,
-    pre_title: { text: "Starting at", price: 999 },
-    title: "The best note book collection 2023",
-    subtitle: {
-      text_1: "Exclusive offer ",
-      percent: 10,
-      text_2: "off this week",
-    },
-    img: "/img/slider/slider-img-3.png",
-    is_light: true,
-  },
+  { id: 1, img: "/img/slider/Banner-Slider-Home-Cube-Gaming_INDIGO-1698491571.jpg" },
+  { id: 2, img: "/img/slider/Banner-Slider-Home-Monitor-CG-F27FV--1694843701.jpg" },
+  { id: 3, img: "/img/slider/Banner-Slider-Home-Monitor-CG-IRIS-GF27QI-1696241749.jpg" },
 ];
 let isActive = ref<boolean>(false);
 const handleActiveIndex = (index: number) => {
